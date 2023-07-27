56e32aae73a2b2fc9158a0be7b16e422d872170a
KarimRC <63407253+karim-rkha@users.noreply.github.com>
2023-07-27 12:36:53 +0100

    basic setup express

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,17 @@
-console.log('Task Manager App')
+const express= require('express')
+const app= express()
+const tasks= require('./routes/tasks')
+
+const port= 3000
+
+app.use(express.json())
+
+app.get('/hello', (req,res)=>{
+    res.send('Hello')
+})
+
+
+app.use('/api/v1/tasks',tasks)
+
+app.listen(port, console.log(`Listening to port ${port}`))
+
